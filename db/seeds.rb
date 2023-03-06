19fa6f166c12f965fdc9822b769485e09af6135e
Jamila <jamnjeri@gmail.com>
2023-03-06 10:21:18 +0300

    Performed the necessary migrations and seeded the prodcution database

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,23 +3,28 @@ puts "🌱 Seeding data..."
 Meme.create([
     {
         name: "Drake Hotline Bling",
-        url: "https://i.imgflip.com/30b1gx.jpg"
+        url: "https://i.imgflip.com/30b1gx.jpg",
+        user_id: 1
     },
     {
         name: "Distracted Boyfriend",
-        url: "https://i.imgflip.com/1ur9b0.jpg"
+        url: "https://i.imgflip.com/1ur9b0.jpg",
+        user_id: 1
     },
     {
         name: "Running Away Balloon",
-        url: "https://i.imgflip.com/261o3j.jpg"
+        url: "https://i.imgflip.com/261o3j.jpg",
+        user_id: 1
     },
     {
         name: "UNO Draw 25 Cards",
-        url: "https://i.imgflip.com/3lmzyx.jpg"
+        url: "https://i.imgflip.com/3lmzyx.jpg",
+        user_id: 1
     },
     {
         name: "Left Exit 12 Off Ramp",
-        url: "https://i.imgflip.com/22bdq6.jpg"
+        url: "https://i.imgflip.com/22bdq6.jpg",
+        user_id: 1
     }
 ])
 
